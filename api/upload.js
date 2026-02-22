822e7c1b00e0b5486fa92aa77b1d9ad9b41bd5fd
byurlee5-prog <byurlee5@gmail.com>
2026-02-22 17:51:55 +0900

    update site

--- api/upload.js
+++ api/upload.js
@@ -1,6 +1,6 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
   if (req.method === "POST") {
-    res.status(200).json({ message: "업로드 성공! (카카오톡 전송은 나중에 추가)" });
+    res.status(200).json({ message: "업로드 성공!" });
   } else {
     res.status(405).json({ error: "Method Not Allowed" });
   }
